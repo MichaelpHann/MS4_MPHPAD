1414ef45c0d694af74febfc449e5b0a0a571542e
MichaelpHann <michael.hann@live.co.uk>
2021-06-04 16:21:52 +0000

    Adding homepage format, styling and functionality

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
     $(".sidenav").sidenav();
     $('.dropdown-trigger').dropdown();
+    $('.materialboxed').materialbox();
+    $('.parallax').parallax();
+    $('.tabs').tabs();
 });
