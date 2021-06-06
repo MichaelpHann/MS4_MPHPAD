fc22cdb50e57a42ad9d2e2d0a7d821b324f44e67
MichaelpHann <michael.hann@live.co.uk>
2021-06-06 09:04:23 +0000

    Added Product and Category models

---
+++ products/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=250, null=True, blank=True)
+    name = models.CharField(max_length=250)
+    description = models.TextField()
+    is_digital = models.BooleanField(default=False, null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
