b3d32e1d87681e764e48229e790094fb97a059be
MichaelpHann <michael.hann@live.co.uk>
2021-06-08 13:41:57 +0000

    Added shopping bag app, urls and template

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -4,4 +4,11 @@ $(document).ready(function(){
     $('.materialboxed').materialbox();
     $('.parallax').parallax();
     $('.tabs').tabs();
+    $('.scrollspy').scrollSpy();
+    $('.slider').slider({
+        indicators: false,
+        height: 600,
+        transition: 500,
+        interval: 6000
+    });
 });
