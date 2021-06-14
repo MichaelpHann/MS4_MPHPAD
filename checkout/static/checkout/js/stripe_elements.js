9e4f5ed84a16ffd8107bd769970dde36be2137c2
MichaelpHann <michael.hann@live.co.uk>
2021-06-14 21:33:08 +0000

    Added Stripe elements

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -6,11 +6,11 @@
     As well as the Boutique Ado Project from Code Institue Full Stack Frameworks
 */
 
-var stripePublicKey = $("#id_stripe_public_key").text().slice(1, -1);
+var stripe_public_key = $("#id_stripe_public_key").text().slice(1, -1);
 var client_secret = $("#id_client_secret").text().slice(1, -1);
-var stripe = Stripe(stripePublicKey);
+var stripe = Stripe(stripe_public_key);
 var elements = stripe.elements();
-var card = elements.create("card");
+
 
 var style = {
     base: {
@@ -29,4 +29,5 @@ var style = {
     }
   };
 
-card.mount("#card-element", {stripe: style});
+var card = elements.create("card", {style: style});
+card.mount("#card-element");
