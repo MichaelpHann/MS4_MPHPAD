af4153a445755f24070e45a948ca94678b01512d
MichaelpHann <michael.hann@live.co.uk>
2021-06-20 13:05:10 +0000

    Implementing adding product functionality

--- products/views.py
+++ products/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+
 from .models import Product, Category
+from .forms import ProductForm
 
-
-# Create your views here.
 
 
 def all_products(request):
@@ -32,3 +32,26 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+
+def add_product(request):
+    """
+    Add a product to the store
+    """
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            product = form.save()
+            # messages.success(request, 'Product successfully added!')
+            return redirect(reverse('add_product'))
+        # else:
+            # messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = ProductForm()
+    
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
