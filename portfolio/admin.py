6c2a98ad4841b539d022dd194283735b52cde975
MichaelpHann <michael.hann@live.co.uk>
2021-06-25 08:12:01 +0000

    Customised Project and Project Category Admin

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -2,6 +2,25 @@ from django.contrib import admin
 from .models import Project, ProjectCategory
 
 
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'category',
+        'name',
+        'descriptive_name',
+        'description',
+        'date',
+        'location',
+        'image',
+    )
 
-admin.site.register(Project)
-admin.site.register(ProjectCategory)
+    ordering = ('sku',)
+
+
+class ProjectCategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+    )
+
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(ProjectCategory, ProjectCategoryAdmin)
